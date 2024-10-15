ed6881468b674035c4cbcf1c7a81f13ef9d20103
fer191820 <147674144+fer191820@users.noreply.github.com>
2024-10-15 20:22:10 -0300

    Update conexionBD.php

--- php/conexionBD.php
+++ php/conexionBD.php
@@ -7,7 +7,7 @@ $bd="conteoasistencia";
 $conn=new mysqli($host,$usuario,$password,$bd);
 
 if ($conn->connect_error){
-    die("conexion errornea". $conn->connect_error);
+    die("conexion errornea a la base de datos". $conn->connect_error);
 } else {
     echo "Conexion exitosa a la base de datos";
 }
